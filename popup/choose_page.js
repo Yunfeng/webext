10636e9e0a661e220d468d5f239123e5f00f6b60
Yunfeng <yfdai@buk.cn>
2018-05-16 21:15:20 +0800

    show data in select

--- popup/choose_page.js
+++ popup/choose_page.js
@@ -1,18 +1,18 @@
 let username;
 
-document.addEventListener("click", function(e) {
-  if (!e.target.classList.contains("page-choice")) {
-    return;
-  }
+// document.addEventListener("click", function(e) {
+//   if (!e.target.classList.contains("page-choice")) {
+//     return;
+//   }
 
-  console.log(e)
+//   console.log(e)
 
-  var chosenPage = "https://" + e.target.textContent;
-  browser.tabs.create({
-    url: chosenPage
-  });
+//   var chosenPage = "https://" + e.target.textContent;
+//   browser.tabs.create({
+//     url: chosenPage
+//   });
 
-});
+// });
 
 
 let popupPort = browser.runtime.connect({name:"port-popup"});
@@ -94,15 +94,36 @@ function checkLoginStatus() {
 			$("#divWork").show();
 			$("#divLogin").hide();
       $("#currentUsername").text(username);
+      refreshData();
 		} else {
       $("#divWork").hide();
       $("#divLogin").show();
       refreshCapchaImage();
     }
-		// alert(v);
-		// if (v.status !== 'OK') {
-		// 	// $("#errmsg").text(v.errMsg);
-		// 	showErrMsg(v.errMsg);
-		// }
 	})
+}
+
+function refreshData() {
+  const params = {
+    'sc.carrier': 'MU',
+    'sc.pageNo': 1,
+    'sc.pageSize': 10,
+    'sc.status': 0
+  }
+  const url = WEBAPP + '/charterFlight/list'
+  $.post(url, params, function(v) {
+    console.log(v.dataList);
+    console.log(v.dataList.length);
+
+    $("#teamNameSelect").empty();
+
+    $.each(v.dataList, function(index, info) {
+      console.log(index);
+      console.log(info);
+      const option = $("<option>").val(info.id).text(info.name);
+      console.log(option);
+      $("#teamNameSelect").append(option);
+    })
+  })
+
 }
