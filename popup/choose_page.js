5b6b657efb3353fe6ec589afb3f5631397c4c8a3
yfdai <1410439635@qq.com>
2018-05-16 23:53:41 +0800

    ready: content gotten

--- popup/choose_page.js
+++ popup/choose_page.js
@@ -52,26 +52,8 @@ const WEBAPP = 'http://tms-2.90sky.com/Flight';
 $(function() {
 	checkLoginStatus();
 
-	$("#btnLogin").click(function() {
-		
-		const url = WEBAPP + "/ajaxLogin.do";
-		const params = {
-			"username": $("#username").val(),
-			"password": $("#password").val(),
-			"captchaValue": $("#captchaCode").val()
-		}
-		console.log(params);
-
-		$.post(url, params, function(v) {
-			console.log(v)
-			// alert(v);
-			if (v.status !== 'OK') {
-				// $("#errmsg").text(v.errMsg);
-				showErrMsg(v.errMsg);
-			}
-		})
-	});
-
+	$("#btnLogin").click(login);
+  $("#btnData").click(autoFillForm);
 	$("#captchaImage").click(refreshCapchaImage);
 });
 
@@ -86,11 +68,14 @@ function refreshCapchaImage() {
 }
 
 function checkLoginStatus() {
+  showErrMsg("状态检测中");
+
 	const url = WEBAPP + "/checkLoginStatus";
 	$.post(url, function(v) {
+    showErrMsg('');
     console.log(v);
 		if (v.status === 'OK') {
-      username = v.desc;
+  		username = v.desc;
 			$("#divWork").show();
 			$("#divLogin").hide();
       $("#currentUsername").text(username);
@@ -99,8 +84,28 @@ function checkLoginStatus() {
       $("#divWork").hide();
       $("#divLogin").show();
       refreshCapchaImage();
+      // $("#errmsg").text("请先登入");
     }
 	})
+}
+
+function login() {
+  const url = WEBAPP + "/ajaxLogin.do";
+  const params = {
+    "username": $("#username").val(),
+    "password": $("#password").val(),
+    "captchaValue": $("#captchaCode").val()
+  }
+  console.log(params);
+
+  $.post(url, params, function(v) {
+    console.log(v)
+    // alert(v);
+    if (v.status !== 'OK') {
+      // $("#errmsg").text(v.errMsg);
+      showErrMsg(v.errMsg);
+    }
+  })  
 }
 
 function refreshData() {
@@ -113,17 +118,58 @@ function refreshData() {
   const url = WEBAPP + '/charterFlight/list'
   $.post(url, params, function(v) {
     console.log(v.dataList);
-    console.log(v.dataList.length);
+    // console.log(v.dataList.length);
 
     $("#teamNameSelect").empty();
 
     $.each(v.dataList, function(index, info) {
-      console.log(index);
-      console.log(info);
+      // console.log(index);
+      // console.log(info);
       const option = $("<option>").val(info.id).text(info.name);
-      console.log(option);
+      // console.log(option);
       $("#teamNameSelect").append(option);
     })
   })
+}
 
+function autoFillForm() {
+  // 开发自动填写表单
+  // showErrMsg();
+  // console.log();
+  const url = WEBAPP + '/charterFlight/detail/' + $("#teamNameSelect").val();
+  $.post(url, function(v) {
+    console.log(v);
+    popupPort.postMessage({to: 'content',
+      action: "reset"
+    });
+
+    const content = {
+      'count': v.totalSeats,
+      'linkMan': v.linkMan,
+      'mobile': v.contactMobile,
+      'teamName': v.name,
+      'flights': []
+    }
+
+    for (const flt0 of v.details) {
+      const flt2 = flt0.flight
+      const flt = {
+          'flightNo': flt2.flightNo,
+          'ddate': flt2.departureDate,
+          'dport': flt2.departureAirport,
+          'aport': flt2.arrivalAirport
+      }
+      content.flights.push(flt);
+    }
+
+    console.log(content);
+
+    popupPort.postMessage({to: 'content',
+      action: "data",
+      content,
+      greeting: "auto fill form"
+    });
+
+
+  })  
 }
